80522db1d2262f32aa24baf7b2b9dd9b1d22ae35
ConstantineT <kiivnv@gmail.com>
2020-01-28 00:46:52 +0200

    Simplify Person

--- src/main/java/com/github/constantinet/assertjandhamcrestshowcase/ParentRelation.java
+++
@@ -1,6 +0,0 @@
-package com.github.constantinet.assertjandhamcrestshowcase;
-
-public enum ParentRelation {
-
-    MOTHER, FATHER
-}
--- src/main/java/com/github/constantinet/assertjandhamcrestshowcase/Person.java
+++ src/main/java/com/github/constantinet/assertjandhamcrestshowcase/Person.java
@@ -1,7 +1,6 @@
 package com.github.constantinet.assertjandhamcrestshowcase;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 /**
  * Mutable representation of a person.
@@ -16,8 +15,7 @@ public final class Person {
     private double height;
 
     private Person spouse;
-    private final Map<ParentRelation, Person> parents = new HashMap<>();
-    private final Set<Person> children = new HashSet<>();
+    private final Set<String> childrenNames = new HashSet<>();
 
     public Person(final int idNumber) {
         this.idNumber = idNumber;
@@ -50,32 +48,12 @@ public final class Person {
     }
 
     /**
-     * Changes first name and last name.
-     *
-     * @param firstName first name
-     * @param lastName last name
-     * @return this for chaining
-     */
-    public Person changeName(final String firstName, final String lastName) {
-        Objects.requireNonNull(firstName);
-        Objects.requireNonNull(lastName);
-        if (firstName.isEmpty() || lastName.isEmpty()) {
-            throw new IllegalArgumentException("Names can not be empty");
-        }
-
-        this.firstName = firstName;
-        this.lastName = lastName;
-
-        return this;
-    }
-
-    /**
-     * Adds height.
+     * Sets height.
      *
      * @param height height in cm and mm
      * @return this for chaining
      */
-    public Person addHeight(final double height) {
+    public Person setHeight(final double height) {
         if (height <= 0.0) {
             throw new IllegalArgumentException("Height must be positive");
         }
@@ -86,44 +64,15 @@ public final class Person {
     }
 
     /**
-     * Add a person as a spouse. Adds {@code this} as a spouse of passed person.
+     * Add a person as a spouse.
      *
      * @param person person
      * @return this for chaining
      */
-    public Person addSpouse(final Person person) {
+    public Person setSpouse(final Person person) {
         Objects.requireNonNull(person);
 
         this.spouse = person;
-        person.spouse = this;
-
-        return this;
-    }
-
-    /**
-     * Add a person with {@code ParentRelation.FATHER}. Adds {@code this} as a children to passed person.
-     *
-     * @param person person
-     * @return this for chaining
-     */
-    public Person addFather(final Person person) {
-        Objects.requireNonNull(person);
-
-        addParent(ParentRelation.FATHER, person);
-
-        return this;
-    }
-
-    /**
-     * Add a person with {@code ParentRelation.MOTHER}. Adds {@code this} as a children to passed person.
-     *
-     * @param person person
-     * @return this for chaining
-     */
-    public Person addMother(final Person person) {
-        Objects.requireNonNull(person);
-
-        addParent(ParentRelation.MOTHER, person);
 
         return this;
     }
@@ -165,6 +114,15 @@ public final class Person {
     }
 
     /**
+     * Returns height.
+     *
+     * @return height
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
      * Returns initials.
      *
      * @return initials from first name and lastname
@@ -178,69 +136,21 @@ public final class Person {
     }
 
     /**
-     * Returns unmodifiable view of parents.
+     * Returns spouse.
      *
-     * @return map of {@link ParentRelation} to {@link Person}
+     * @return Optional of spouse or empty optional
      */
-    public Map<ParentRelation, Person> getParents() {
-        return Collections.unmodifiableMap(parents);
+    public Optional<Person> getSpouse() {
+        return Optional.ofNullable(spouse);
     }
 
     /**
-     * Returns unmodifiable view of children.
+     * Returns unmodifiable view of children names.
      *
      * @return set of {@link Person}
      */
-    public Set<Person> getChildren() {
-        return Collections.unmodifiableSet(children);
-    }
-
-    /**
-     * Returns an ID number of the spouse.
-     *
-     * @return optional of ID or empty optional if no info available
-     */
-    public Optional<Integer> getSpouseId() {
-        return extractId(spouse);
-    }
-
-    /**
-     * Returns an ID number of a parent with relation {@code ParentRelation.FATHER}.
-     *
-     * @return optional of ID or empty optional if no info available
-     */
-    public Optional<Integer> getFatherId() {
-        return extractId(parents.get(ParentRelation.FATHER));
-    }
-
-    /**
-     * Returns an ID number of a parent with relation {@code ParentRelation.MOTHER}.
-     *
-     * @return optional of ID or empty optional if no info available
-     */
-    public Optional<Integer> getMotherId() {
-        return extractId(parents.get(ParentRelation.MOTHER));
-    }
-
-    /**
-     * Returns all available relatives.
-     *
-     * @return iterable of {@link Person}
-     */
-    public Stream<Person> getAllRelatives() {
-        final Stream<Person> parentsAndChildrenStream = Stream.concat(parents.values().stream(), children.stream());
-        final Stream<Person> spouseStream = spouse == null ? Stream.empty() : Stream.of(spouse);
-        return Stream.concat(parentsAndChildrenStream, spouseStream);
-    }
-
-    private void addParent(final ParentRelation parentRelation, final Person person) {
-        this.parents.put(parentRelation, person);
-        person.children.add(this);
-    }
-
-    private Optional<Integer> extractId(final Person person) {
-        return Optional.ofNullable(person)
-                .map(Person::getIdNumber);
+    public Set<String> getChildrenNames() {
+        return Collections.unmodifiableSet(childrenNames);
     }
 
     @Override
@@ -254,5 +164,18 @@ public final class Person {
     @Override
     public int hashCode() {
         return Objects.hash(idNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "idNumber=" + idNumber +
+                ", firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", height=" + height +
+                ", spouse=" + spouse +
+                ", childrenNames=" + childrenNames +
+                '}';
     }
 }
